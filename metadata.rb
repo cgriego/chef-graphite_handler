5215a6d4df1faaeba1e98acdfa597a5c759994fb
Morgan Nelson <mnelson@steele-forge.com>
2015-05-12 15:37:22 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "MIT"
 description      "Installs/Configures chef-handler-graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.0"
+name             "graphite_handler"
 
 depends "chef_handler"
 suggests "chef_gem"
